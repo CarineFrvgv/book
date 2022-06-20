309859822217bed93ea360a3112a91d73d43c702
CarineFrvgv <93139372+CarineFrvgv@users.noreply.github.com>
2022-06-19 23:25:19 -0300

    add body-parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => 
