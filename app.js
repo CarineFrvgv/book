15c1e67ef9d4c499a199c94ab9baf6f6352434a7
CarineFrvgv <93139372+CarineFrvgv@users.noreply.github.com>
2022-06-19 23:34:56 -0300

    set view engine, serve static files

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const pathJoin = require('path').join
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(pathJoin(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
 
 const PORT = process.env.PORT || 3000;
 
